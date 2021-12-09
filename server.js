4586daf2e6cc45db9704774797caeac870a5bb05
LazyLizard95 <51461957+LazyLizard95@users.noreply.github.com>
2021-12-09 12:08:40 -0500

    Added git routes & simple post route

--- server.js
+++ server.js
@@ -3,12 +3,24 @@ const path = require('path');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const notes = require('./db/db.json');
 //parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
 //Middleware that staticly gives browser all of public
 app.use(express.static("public"))
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+})
+
+app.post('/api/notes', (req, res) => {
+    const newNote = req.body;
+    notes.push(newNote);
+    res.json(notes);
+})
+
 app.get('/', (req, res) =>{
     res.sendFile(path.join(__dirname, "./public/index.html"))
 })
