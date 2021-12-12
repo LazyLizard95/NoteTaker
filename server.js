12b3c360d37d4a8d647119df96bf28af58eb737f
LazyLizard95 <51461957+LazyLizard95@users.noreply.github.com>
2021-12-12 12:51:31 -0500

    one more thing

--- server.js
+++ server.js
@@ -29,11 +29,11 @@ app.post('/api/notes', (req, res) => {
 
 app.delete('/api/notes/:id', (req, res) => {
     const noteId = req.params.id
-    fs.readFile(path.join(__dirname, './db/db.json'), JSON.stringify(notes), (err) =>{
-        
+    fs.readFile(path.join(__dirname, './db/db.json'), JSON.stringify(notes), (err) => {
         if (err) throw err;
-        res.json(noteId);
+        notes.find(noteId);
     })
+    
     
     
 })
